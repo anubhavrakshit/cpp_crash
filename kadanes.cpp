40d4d3e9a14b5ec03b159c45667c05d6b3686c68
Anubhav Rakshit <anubhavrocks@gmail.com>
2020-04-05 14:56:53 +0530

    Kadanes algo

---
+++ kadanes.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int globalMax, localMax;
+        globalMax = localMax = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            localMax = max(localMax + nums[i], nums[i]);
+            if (localMax > globalMax) {
+                globalMax = localMax;
+            }
+        }
+        
+        return globalMax;
+    }
+};
+
+
+int main() {
+    vector <int> nums{-2,1,-3,4,-1,2,1,-5,4};
+    Solution s;
+    cout << "maxSubArray = " << s.maxSubArray(nums) << "\n";
+    return 0;
+}
