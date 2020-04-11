24a72f33313b2e884bbe3b6c24cd06369eadffe3
Anubhav Rakshit <anubhavrocks@gmail.com>
2020-04-12 00:31:49 +0530

    Update algo

--- rail.cpp
+++ rail.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
 
@@ -9,44 +10,35 @@ bool comp(int a, int b)
     return (a < b); 
 } 
 int min_cost(vector<int>&days, vector<int>& costs) {
-    int min_cost = 0;
 
-    vector<vector<int> > dp(2);
-    for(auto x: days) {
-        dp[0].push_back(x);
-        dp[1].push_back(0);
-    }
+    int dp[366]{};
 
-    int min_pass_cost = std::min({costs[0], costs[1], costs[2]}, comp);
-    
-    // pre fill; we know at starting min cost will be min(cost[0], cost[1]...)
-    dp[1][0] = min_pass_cost;
+    // Calculate for min cost if you travel each day
+    // Cost will stay unchanged if you did not travel on that day as per days<>
+    // Also take care of 7 and 30 day passes 
+    // If you had purchased a 7 day pass cost with cost[1] + dp[i - 7]
 
-    cout << std::endl;
-    for (int j = 1; j < days.size(); j++) {
-        // The min cost will be min of:
-        // dp[1][j - 1] + cost[0]
-        // ((dp[0][j] -dp[0][0] ) / 7) * cost[1]  + cost[1]
-        // (dp[0][j] -dp[0][0] ) /  30) * cost[2] + cost[2]
+    for (int i = 1; i < 366; i++) {
+        if (find(days.begin(),days.end(), i) == days.end()) {
+            dp[i] = dp[i - 1];
+        } else {
+            int min1 = dp[i - 1] + costs[0];
+            int min2 = dp[max(i - 7, 0)] + costs[1];
+            int min3 = dp[max(i - 30, 0)] + costs[2];
+            dp[i] = min({min1, min2, min3}, comp);
+            cout << "\nday = " << i << " min1 = " << min1 << " min2 = " << min2 << " min3 = " << min3 << std::endl;
+        }
 
-        int min1 = dp[1][j - 1] + costs[0];
-        int min2 = (((dp[0][j] - dp[0][0]) / 7) * costs[1]) + costs[1];
-        int min3 = (((dp[0][j] - dp[0][0]) / 30) * costs[2]) + costs[2];
-
-        dp[1][j] = std::min({min1, min2, min3}, comp);
-        cout << "min1 = " << min1 << " min2 = " << min2 << " min3 = " << min3 << endl;
-        cout << "cost of day " << dp[0][j] << " is " << dp[1][j] << std::endl;
     }
 
 
-
-    return dp[1][dp[1].size() - 1];
+    return dp[365];
 }
 
 int main() 
 {
-    vector<int> days{1,2,3,4,5,6,7,8,9,10,30,31};
-    vector<int> costs{2,7,15};
+    vector<int> days{1,4,6,9,10,11,12,13,14,15,16,17,18,20,21,22,23,27,28};
+    vector<int> costs{3, 13, 45};
 
     cout << "min cost = " << min_cost(days, costs) << std::endl;
 
