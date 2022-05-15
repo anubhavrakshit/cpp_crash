78fa2a237f3ad29ded7a103cdf1a51b62dddafe5
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2022-05-15 17:34:41 +0530

    Combine all conditions

--- aoc_day_5_1.cpp
+++ aoc_day_5_1.cpp
@@ -6,86 +6,78 @@
 #include <map>
 using namespace std;
 
-int main() {
-    struct line {
-        pair<int, int> start;
-        pair<int, int> end;
-        line(int x1, int y1, int x2, int y2) : start{x1, y1}, end{x2, y2} {}
-        bool is_equal_x() {
-            return (start.first == end.first);
+struct line
+{
+    pair<int, int> start;
+    pair<int, int> end;
+    line(int x1, int y1, int x2, int y2) : start{x1, y1}, end{x2, y2} {}
+    bool is_equal_x()
+    {
+        return (start.first == end.first);
+    }
+    bool is_equal_y()
+    {
+        return (start.second == end.second);
+    }
+    void print()
+    {
+        cout << start.first << " : " << start.second << " | " << end.first << " : " << end.second << endl;
+    }
+
+    vector<pair<int, int>> points()
+    {
+        vector<pair<int, int>> ps;
+        bool is_diag{false};
+
+        if (abs(start.first - end.first) == abs(start.second - end.second))
+        {
+            is_diag = true;
         }
-        bool is_equal_y() {
-            return (start.second == end.second);
-        }        
-        void print() {
-            cout << start.first << " : " << start.second << " | " << end.first << " : " << end.second << endl;
-        }
-
-        vector<pair<int, int>> points() {
-            vector<pair<int, int>> ps;
-            bool is_diag{false};
-
-            if (abs(start.first - end.first) == abs(start.second - end.second)) {
-                is_diag = true;
-            }
-            if (is_diag) {
-                // x1, y1 -> x2, y2
-                // x1 < x2
-                // x1 > x2
-                // y1 < y2 
-                // y1 > y2 
-                auto x1 = start.first;
-                auto y1 = start.second;
-                auto x2 = end.first;
-                auto y2 = end.second;
-                while (x1 != x2 && y1 != y2) {
-                    ps.push_back({x1, y1});
-                    if (x1 < x2) {
-                        x1++;
-                    } else {
-                        x1--;
-                    }
-
-                    if (y1 < y2) {
-                        y1++;
-                    } else {
-                        y1--;
-                    }
-
+        if (is_diag or is_equal_x() or is_equal_y())
+        {
+            // x1, y1 -> x2, y2
+            // x1 < x2
+            // x1 > x2
+            // y1 < y2
+            // y1 > y2
+            auto x1 = start.first;
+            auto y1 = start.second;
+            auto x2 = end.first;
+            auto y2 = end.second;
+            while (x1 != x2 or y1 != y2)
+            {
+                ps.push_back({x1, y1});
+                if (x1 < x2)
+                {
+                    x1++;
                 }
-                ps.push_back({x2, y2});
-            } else if (is_equal_x()) {
-                int i = start.first;
-                int j = start.second;
-                int k = end.second;
-                if (j > k) {
-                    swap(j, k);
+                else if (x1 > x2)
+                {
+                    x1--;
                 }
 
-                for (int l = j; l <= k; l++) {
-                // cout << i << " , " << l << endl;
-                    ps.push_back({i, l});
+                if (y1 < y2)
+                {
+                    y1++;
                 }
-            } else if (is_equal_y()) {
-                int i = start.second;
-                int j = start.first;
-                int k = end.first;
-                if (j > k) {
-                    swap(j, k);
-                }
-
-                for (int l = j; l <= k; l++) {
-                    // cout << i << " , " << l << endl;
-                    ps.push_back({l, i});
+                else if (y1 > y2)
+                {
+                    y1--;
                 }
             }
-            return ps;
-        } 
-    };
+            ps.push_back({x2, y2});
+        }
+        
+        return ps;
+    }
+};
 
+int main()
+{
     string lstr;
     vector<line> lines;
-    while (getline(cin, lstr)) {
+    while (getline(cin, lstr))
+    {
         stringstream stream(lstr);
         int x1, y1, x2, y2;
         char comma, dash, brkt;
@@ -95,19 +87,23 @@ int main() {
 
     int ans{0};
     map<pair<int, int>, int> intersect;
-    for (auto line : lines) {
+    for (auto line : lines)
+    {
         auto points = line.points();
-        for (auto p : points) {
+        for (auto p : points)
+        {
             intersect[p]++;
-        }         
+        }
     }
-    
-    for (auto kv : intersect) {
-        if (kv.second >= 2) {
+
+    for (auto kv : intersect)
+    {
+        if (kv.second >= 2)
+        {
             ans++;
         }
     }
 
     cout << "Ans = " << ans << endl;
-    return 0;    
+    return 0;
 }
