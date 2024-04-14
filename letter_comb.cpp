4b0618ffa2436a8a96e904c592655f4d21d42715
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2024-04-14 13:48:43 +0530

    Keypad to letters

---
+++ letter_comb.cpp
@@ -0,0 +1,34 @@
+void hlpr(string &digits, size_t pos, string chose,
+          unordered_map<char, vector<char>> &map, vector<string> &ans) {
+  if (pos == digits.length()) {
+    ans.push_back(chose);
+    return;
+  }
+
+  for (auto letter : map[digits[pos]]) {
+    hlpr(digits, pos + 1, chose + letter, map, ans);
+  }
+}
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+       if (digits.empty()) {
+        return {};
+       }
+       unordered_map<char, vector<char>> map = {
+        {'2',{'a', 'b', 'c'}},
+        {'3',{'d', 'e', 'f'}},
+        {'4',{'g', 'h', 'i'}},
+        {'5',{'j', 'k', 'l'}},
+        {'6',{'m', 'n', 'o'}},
+        {'7',{'p', 'q', 'r', 's'}},
+        {'8',{'t', 'u', 'v'}},
+        {'9',{'w', 'x', 'y', 'z'}},
+       };
+       
+       vector<string> ans;
+       string chose;
+       hlpr(digits, 0, chose, map, ans);
+       return ans;
+    }
+};
